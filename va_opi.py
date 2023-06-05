5d920982ab90db2f4618e90903997bc2fa0e958a
Gary McMurtrie S <garymservice@gmail.com>
2023-06-05 11:39:25 -0400

    OPI should now combine relevant lines together

--- va_opi.py
+++ va_opi.py
@@ -20,25 +20,37 @@ x_ypos = x_xpos + w_xpos + gap_h
 va_pvs = open("VA_pvlist.txt", "r")
 raw_data = va_pvs.readlines()
 
-data = []
+data = {}
 for line in raw_data:
-    base = ":".join(line.split(":")[:3])
-    name = line[3:].rstrip()
-    x = base + ":X_RD"
-    y = base + ":Y_RD"
-    data.append((name, x, y))
+    line = line.rstrip()
+    name = ":".join(line.split(":")[1:3])
+
+    if name not in data:
+        data[name] = []
+
+    data[name].append(line)
 
 # Sets up screen
 screen = Display(800, 600, "BPM Readings")
 
-for name, xpos, ypos in data:
-    name_lbl = widgets.Label(x_name, y0, w_name, box_height, name)
-    x_rd_text = TextUpdate(x_xpos, y0, w_xpos, box_height, xpos)
-    y_rd_text = TextUpdate(x_ypos, y0, w_ypos, box_height, ypos)
+for key in data.keys():
+    name_lbl = widgets.Label(x_name, y0, w_name, box_height, key)
     screen.add_child(name_lbl)
-    screen.add_child(x_rd_text)
-    screen.add_child(y_rd_text)
+
+    for child in data[key]:
+        child_text = TextUpdate(x_xpos, y0, w_xpos, box_height, child)
+        screen.add_child(child_text)
+
     y0 += box_height + gap_v
+
+# for name, xpos, ypos in data:
+#     name_lbl = widgets.Label(x_name, y0, w_name, box_height, name)
+#     x_rd_text = TextUpdate(x_xpos, y0, w_xpos, box_height, xpos)
+#     y_rd_text = TextUpdate(x_ypos, y0, w_ypos, box_height, ypos)
+#     screen.add_child(name_lbl)
+#     screen.add_child(x_rd_text)
+#     screen.add_child(y_rd_text)
+#     y0 += box_height + gap_v
 
 # Outputs to file
 r = Renderer(screen)
