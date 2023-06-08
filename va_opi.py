45fa5edf25c25bf0f0bf10aeb44cc9ded00ab78c
Gary McMurtrie S <garymservice@gmail.com>
2023-06-08 14:55:18 -0400

    Updates to formatting and code cleaning

--- va_opi.py
+++ va_opi.py
@@ -6,33 +6,28 @@ from opigen.contrib import TextUpdate
 
 # Variables to control widget positioning
 filename = "va_opi"
-box_height = 25  # Controls how tall boxes are
+box_height = 25
 horizontal_gap = 5
-vertical_gap = 5  # Separation between boxes
+vertical_gap = 5
 label_width = 150
 data_width = 700
 
 # Reads in data from file
-raw_data = open("VA_pvlist.txt", "r").readlines()
+raw_data = open("VA_pvlist.txt", "r", encoding='utf-8').readlines()
 
-# Creates a dictionary for each type, that maps to a dictionary for each name with entries for each widget that must be created.
+# Creates a dictionary for each type, that maps to a dictionary for each name with entries for each
+# widget that must be created.
 data = {}
 for line in raw_data:
     line = line.rstrip()
 
     # Strips numbers from the type, so they can be grouped together
-    type = (line.split(":")[2].split("_")[0].translate(
-        str.maketrans("", "", "0123456789")))
-
-    if type not in data:
-        data[type] = {}
+    component_type = ''.join(ch for ch in line.split(":")[2].split("_")[0]
+                             if not ch.isdigit())
 
     name = ":".join(line.split(":")[1:3])
 
-    if name not in data[type]:
-        data[type][name] = []
-
-    data[type][name].append(line)
+    data.setdefault(component_type, {}).setdefault(name, []).append(line)
 
 # Sets up screen
 screen = Display(800, 600, "Virtual Accelerator")
@@ -42,25 +37,25 @@ x0 = horizontal_gap
 y0 = vertical_gap
 tab_container = widgets.TabbedContainer(x0, y0, label_width + data_width + 10,
                                         box_height * 30)
-tab_container.resize_behaviour = widgets.ResizeBehaviour.RESIZE_CONTAINER_TO_FIT_OPI
 screen.add_child(tab_container)
 
 # Gets all the types and sorts them
-types = [i for i in data.keys()]
-types.sort()
+component_types = list(data)
+component_types.sort()
 
 # Creates a tab for each type of monitor
-for type in types:
+for component_type in component_types:
     y0 = vertical_gap
 
     # Creates a singular widget to add to the tab that contains all other widgets
-    tab_widget = widgets.GroupingContainer(0, 0, label_width + data_width + 10,
-                                           box_height * 30, type)
-    tab_widget.resize_behaviour = widgets.ResizeBehaviour.RESIZE_CONTAINER_TO_FIT_OPI
+    tab_widget = widgets.GroupingContainer(0, 0, label_width + data_width,
+                                           box_height * 29, component_type)
 
     # Gets all the column names to be shown
-    column_names = sorted(
-        [i.split(":")[-1] for i in data[type][next(iter(data[type]))]])
+    column_names = sorted([
+        i.split(":")[-1]
+        for i in data[component_type][next(iter(data[component_type]))]
+    ])
     column_count = len(column_names)
 
     # Creates widget labels for all the column names
@@ -76,14 +71,14 @@ for type in types:
     y0 += box_height + vertical_gap
 
     # This goes through each monitor and adds them to the tab
-    for monitor in data[type]:
+    for monitor in data[component_type]:
         label = widgets.Label(horizontal_gap, y0, label_width, box_height,
                               monitor)
         tab_widget.add_child(label)
 
         x0 = horizontal_gap + label_width + horizontal_gap
 
-        for child in sorted(data[type][monitor]):
+        for child in sorted(data[component_type][monitor]):
             # Boxes for the child widgets are resized to fill horizontally
             if child[-4:] == "CSET":
                 child_widget = TextEntry(
@@ -107,7 +102,7 @@ for type in types:
 
         y0 += box_height + vertical_gap
 
-    tab_container.add_tab(type, tab_widget)
+    tab_container.add_tab(component_type, tab_widget)
 
 # Outputs to file
 r = Renderer(screen)
