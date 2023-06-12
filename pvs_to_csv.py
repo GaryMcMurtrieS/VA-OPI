f52635ac4f7b7055d700a72756130ad4c4ba97ea
Gary McMurtrie S <garymservice@gmail.com>
2023-06-12 10:27:01 -0400

    Major Revisions. Added file to convert PVs list to CSV Updated main file to be more functionally-programmed, code is now much better managed and should be easier to use.

---
+++ pvs_to_csv.py
@@ -0,0 +1,27 @@
+""" File that converts PV list to more functional CSV file """
+
+import pandas as pd
+
+
+def convert_to_csv(txt_file, csv_file):
+    """Converts raw txt file containing PVs to a CSV file """
+    pvs = pd.read_csv(txt_file, header=None)
+
+    pvs = pvs[0].str.split(':', expand=True)
+
+    # Splitting only columns 2 and 3 on underscores, as the final column with variable identifiers
+    # contains underscores but should not be split
+    df1 = pvs[1].str.split('_', expand=True)
+    df2 = pvs[2].str.split('_', expand=True)
+    pvs = pd.concat([pvs[0], df1, df2, pvs.loc[:, 3:]], axis=1)
+
+    pvs.columns = [
+        "System Idenfitier", "Location", "Managing Device", "Device Type", "Position",
+        "Variable Identifier"
+    ]
+
+    pvs.to_csv(csv_file, index=True)
+
+
+if __name__ == "__main__":
+    convert_to_csv("VA_pvlist.txt", "va_pvs.csv")
--- va_opi.py
+++ va_opi.py
@@ -1,3 +1,5 @@
+""" Creates Phoebus and CS-Studio OPI Files for reading Accelerator Data"""
+
 from opigen import Renderer
 from opigen import widgets
 from opigen.contrib import Display
@@ -5,97 +7,131 @@ from opigen.contrib import TextEntry
 from opigen.contrib import TextUpdate
 
 # Variables to control widget positioning
-filename = "va_opi"
-widget_height = 25
-name_width = 150
-data_width = 700
-screen_width = 800
-screen_height = 650
-horizontal_gap = 5
-vertical_gap = 5
+FILENAME = "va_opi"
+WIDGET_HEIGHT = 25
+NAME_WIDTH = 150
+DATA_WIDTH = 700
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 650
+HORIZONTAL_GAP = 5
+VERTICAL_GAP = 5
 
-# Reads in data from file
-raw_data = open("VA_pvlist.txt", "r", encoding='utf-8').readlines()
 
-# Creates a dictionary for each type, that maps to a dictionary for each name with entries for each
-# widget that must be created.
-pv_data = {}
-for line in raw_data:
-    line = line.rstrip()
+def data_reading():
+    """Reads data and converts it to dict format for main code"""
 
-    # Strips numbers from the type, so they can be grouped together
-    device_type = ''.join(ch for ch in line.split(":")[2].split("_")[0] if not ch.isdigit())
+    # Reads in data from file
+    with open("VA_pvlist.txt", "r", encoding='utf-8') as file:
+        raw_data = file.readlines()
 
-    name = ":".join(line.split(":")[1:3])
+    # Creates a dictionary for each type, that maps to a dictionary for each name with entries for
+    # each widget that must be created.
+    pv_data = {}
+    for line in raw_data:
+        line = line.rstrip()
 
-    pv_data.setdefault(device_type, {}).setdefault(name, []).append(line)
+        # Strips numbers from the type, so they can be grouped together
+        device_type = ''.join(ch for ch in line.split(":")[2].split("_")[0] if not ch.isdigit())
 
-# Sets up screen
-screen = Display(screen_width, screen_height, "LS1FS1 Virtual Accelerator")
+        name = ":".join(line.split(":")[1:3])
 
-# Creates the tab container to hold all the various monitors
-x0 = horizontal_gap
-y0 = vertical_gap
-tab_container = widgets.TabbedContainer(x0, y0, name_width + data_width + (3 * horizontal_gap) + 12,
-                                        screen_height + 33)
-screen.add_child(tab_container)
+        pv_data.setdefault(device_type, {}).setdefault(name, []).append(line)
 
-# Gets all the types and sorts them
-device_types = list(pv_data)
-device_types.sort()
+    return pv_data
 
-# Creates a tab for each type of monitor
-for device_type in device_types:
-    y0 = vertical_gap
 
-    # Creates a singular widget to add to the tab that contains all other widgets
-    tab_widget = widgets.GroupingContainer(0, 0,
-                                           name_width + data_width + (3 * horizontal_gap) + 10,
-                                           screen_height, device_type)
+def create_tab_container(screen):
+    """Creates and adds tab container to the screen"""
+    tab_container = widgets.TabbedContainer(HORIZONTAL_GAP, VERTICAL_GAP,
+                                            NAME_WIDTH + DATA_WIDTH + (3 * HORIZONTAL_GAP) + 12,
+                                            SCREEN_HEIGHT + 33)
+    screen.add_child(tab_container)
+    return tab_container
 
-    # Gets names of columns to be shown
+
+def get_device_types(pv_data):
+    """Gets all the device types and sorts them"""
+    device_types = list(pv_data)
+    device_types.sort()
+    return device_types
+
+
+def create_columns_and_get_width(pv_data, tab_widget, device_type):
+    """Creates column labels for the given tab"""
+    # Gets and sorts all column names
     column_names = sorted(
         [i.split(":")[-1] for i in pv_data[device_type][next(iter(pv_data[device_type]))]])
     column_count = len(column_names)
-    column_width = (data_width / column_count) - horizontal_gap
+    column_width = (DATA_WIDTH / column_count) - HORIZONTAL_GAP
 
     # Creates all the widgets for column name labels
-    x0 = horizontal_gap + name_width + horizontal_gap
+    x_0 = HORIZONTAL_GAP + NAME_WIDTH + HORIZONTAL_GAP
     for column_name in column_names:
-        column_label = widgets.Label(x0, y0, column_width, widget_height, column_name)
+        column_label = widgets.Label(x_0, HORIZONTAL_GAP, column_width, WIDGET_HEIGHT, column_name)
         tab_widget.add_child(column_label)
-        x0 += column_width + horizontal_gap
-    y0 += widget_height + vertical_gap
+        x_0 += column_width + HORIZONTAL_GAP
+
+    return column_width
+
+
+def create_tab_widget(pv_data, device_type):
+    """Creates the container that goes into a given tab"""
+    # Creates a singular widget to add to the tab that contains all other widgets
+    tab_widget = widgets.GroupingContainer(0, 0,
+                                           NAME_WIDTH + DATA_WIDTH + (3 * HORIZONTAL_GAP) + 10,
+                                           SCREEN_HEIGHT, device_type)
+
+    # Create column labels
+    column_width = create_columns_and_get_width(pv_data, tab_widget, device_type)
+
+    y_0 = VERTICAL_GAP + WIDGET_HEIGHT + VERTICAL_GAP
 
     # This goes through each actual device that is of type device type
     for device in pv_data[device_type]:
-        x0 = horizontal_gap
+        x_0 = HORIZONTAL_GAP
 
         # First, the label is created and added with the device ID
-        device_label = widgets.Label(x0, y0, name_width, widget_height, device)
+        device_label = widgets.Label(x_0, y_0, NAME_WIDTH, WIDGET_HEIGHT, device)
         tab_widget.add_child(device_label)
 
-        x0 += name_width + horizontal_gap
+        x_0 += NAME_WIDTH + HORIZONTAL_GAP
 
         # Next, each of that devices parameters is added as a widget
         # CSET parameters can take text input and be changed, everything else cannot be changed
         # and only read back
         for parameter in sorted(pv_data[device_type][device]):
             if parameter.endswith("CSET"):
-                parameter_output = TextEntry(x0, y0, column_width, widget_height, parameter)
+                parameter_output = TextEntry(x_0, y_0, column_width, WIDGET_HEIGHT, parameter)
             else:
-                parameter_output = TextUpdate(x0, y0, column_width, widget_height, parameter)
+                parameter_output = TextUpdate(x_0, y_0, column_width, WIDGET_HEIGHT, parameter)
 
             tab_widget.add_child(parameter_output)
-            x0 += column_width + horizontal_gap
+            x_0 += column_width + HORIZONTAL_GAP
 
-        y0 += widget_height + vertical_gap
+        y_0 += WIDGET_HEIGHT + VERTICAL_GAP
 
-    # Once all widgets are added to the container for the tab, the tab is created and the widget is
-    # added to it
-    tab_container.add_tab(device_type, tab_widget)
+    return tab_widget
 
-# Outputs to file
-r = Renderer(screen)
-r.to_opi(f"{filename}.opi")
-r.to_bob(f"{filename}.bob")
+
+def main():
+    """Uses dictionary of PVs to create widgets and then ouput the CS-Studio and Phoebus files"""
+    # Sets up screen
+    screen = Display(SCREEN_WIDTH, SCREEN_HEIGHT, "LS1FS1 Virtual Accelerator")
+
+    # Creates the tab container to hold all the various monitors
+    tab_container = create_tab_container(screen)
+
+    pv_data = data_reading()
+
+    # Creates a tab for each type of monitor
+    for device_type in get_device_types(pv_data):
+        tab_container.add_tab(device_type, create_tab_widget(pv_data, device_type))
+
+    # Outputs to file
+    screen_renderer = Renderer(screen)
+    screen_renderer.to_opi(f"{FILENAME}.opi")
+    screen_renderer.to_bob(f"{FILENAME}.bob")
+
+
+if __name__ == "__main__":
+    main()
